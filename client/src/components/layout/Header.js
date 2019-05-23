198906482dcd3999ebbf4a730a8c304b2bc1ae81
Abdullah Alam <abdullah_alam_omi@outlook.com>
2019-05-23 00:00:27 -0700

    add footer section

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -5,6 +5,7 @@ export default function Header(props) {
     <React.Fragment>
       <hr />
       <span>
+        <br />
         <h1>WHASE</h1>
         <p>Without Whiteboards Hiring All Software Engineers</p>
         <p>Whiteboard interviews are dead because whiteboard interviews suck. So much anxiety, so little time...</p>
